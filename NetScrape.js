988803ccfe4acb90bab5f54ac551231194fe15ed
Jeff Hinzmann <JAHinzmann@gmail.com>
2018-06-23 14:20:24 -0400

    worked out Heroku issues and began building scraper with assistance from Charles Dunn

---
+++ NetScrape.js
@@ -0,0 +1,8 @@
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+var port = process.env.PORT||3000;
